60c7fe3a865e01f1ca0f73b1b0e1103d66de13a1
regeldso <rvcoding@gmail.com>
2017-07-17 22:22:54 +0300

    Author collection controller

---
+++ RESTful/src/Library.API/Controllers/AuthorCollectionsController.cs
@@ -0,0 +1,43 @@
+﻿using Library.API.Entities;
+using Library.API.Models;
+using Library.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.API.Controllers
+{
+    [Route("api/authorcollections")]
+    public class AuthorCollectionsController: Controller
+    {
+        private ILibraryRepository _libraryRepository;
+
+        public AuthorCollectionsController(ILibraryRepository libraryRepositry)
+        {
+            _libraryRepository = libraryRepositry;
+        }
+        [HttpPost]
+        public IActionResult CreateAuthorCollection([FromBody] IEnumerable<AuthorForCreationDto> authorCollection)
+        {
+            if (authorCollection == null)
+            {
+                return BadRequest();
+            }
+            var authorEntities = AutoMapper.Mapper.Map<IEnumerable<Author>>(authorCollection);
+            foreach (var author in authorEntities)
+            {
+
+                _libraryRepository.AddAuthor(author);
+            }
+            if(!_libraryRepository.Save())
+            {
+                throw new Exception("Creating an author collection failed on save");
+            }
+            return Ok();
+        }
+
+    }
+}
--- RESTful/src/Library.API/Models/AuthorForCreationDto.cs
+++ RESTful/src/Library.API/Models/AuthorForCreationDto.cs
@@ -12,5 +12,7 @@ namespace Library.API.Models
         public string LastName { get; set; }
         public DateTimeOffset DateOfBirth { get; set; }
         public string Genre { get; set; }
+        public ICollection<BookForCreationDto> Books { get; set; }
+        = new List<BookForCreationDto>();
     }
 }
